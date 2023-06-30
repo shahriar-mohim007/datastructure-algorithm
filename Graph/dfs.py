242f50b57fa750df22cea3ab324eae452c51a7b8
Nasif Shahriar Mohim <53572742+shahriarnasif@users.noreply.github.com>
2023-06-30 12:02:47 +0600

    data structure and algorithm

---
+++ Graph/dfs.py
@@ -0,0 +1,34 @@
+graph = [[] for _ in range(100)]
+visited = [False]*6
+
+
+def dfs(s):
+    
+    visited[s] = True
+    for i in graph[s]:
+        if not visited[i]:
+         dfs(i)
+
+
+def main():
+   global graph,visited
+   edge = int(input("Enter the number of edges in the graph: "))
+   node = int(input("Enter the number of nodes in the graph: "))
+   for _ in range(edge):
+       x,y = map(int, input().split())
+       
+       graph[x].append(y)
+       graph[y].append(x)
+          
+   connectedComponents = 0
+   for i in range(1,node+1):
+        if not visited[i]:
+            dfs(i)
+            print(visited)
+            connectedComponents += 1
+       
+   print("Number of connected components:", connectedComponents)
+   return 0
+
+if __name__ == '__main__':
+   main()    
